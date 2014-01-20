b61b0f973421ada308bcc45cb59650aa52ac0478
yigitgunay <yigit.guenay@rwth-aachen.de>
2014-01-20 19:05:08 +0100

    Removed unnecessary methods from L2PFetcher. Modified code for fetching starting and ending times of all courses.

--- Xcode Project/Skedify/Skedify/L2PFetcher.m
+++ Xcode Project/Skedify/Skedify/L2PFetcher.m
@@ -18,7 +18,8 @@ enum _requestState {
     announcement = 2,
     pdf = 3,
     documents = 4,
-    courseRooms = 5
+    courseRooms = 5,
+    courseDates = 6
 };
 
 typedef enum _requestState requestState;
@@ -26,6 +27,8 @@ typedef enum _requestState requestState;
 @interface L2PFetcher ()
 {
     bool parseData;
+    NSMutableArray* IDs;
+    NSMutableArray* dates;
 }
 
 @property(retain) NSURLConnection   *urlConnection;
@@ -48,9 +51,49 @@ typedef enum _requestState requestState;
         self.accessToken = nil;
         self.elementNameFromXMLParser = nil;
         self.state = notSet;
+        IDs = [NSMutableArray new];
+        dates = [NSMutableArray new];
     }
     return self;
 }
+
+-(void)getDatesForL2PCourse:(NSString*) courseID
+{
+    self.state = courseDates;
+    
+    // NOTE: See list of available endpoints in rwth_aachen_app_guideline_v04.pdf
+    NSString *url =
+    @"https://www2.elearning.rwth-aachen.de/L2PWebservices/L2PPublicDomainService.asmx";
+    
+    
+    // NOTE: Open the URL of the endpoint in a web browser for the list of available
+    //       actions and HTTP request and response examples.
+    NSString *action = @"http://cil.rwth-aachen.de/l2p/services/GetAppointments";
+    
+    
+    NSString *bodyString =
+    [NSString stringWithFormat:
+     @"<?xml version=\"1.0\" encoding=\"utf-8\"?>\
+     <soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\
+     <soap:Body>\
+     <GetAppointments xmlns=\"http://cil.rwth-aachen.de/l2p/services\"> \
+     <userToken>%@</userToken> \
+     <courseId>%@</courseId> \
+     </GetAppointments> \
+     </soap:Body> \
+     </soap:Envelope>",
+     self.accessToken, courseID];
+    
+    NSMutableURLRequest *courseInfoRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
+    [courseInfoRequest addValue:@"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
+    [courseInfoRequest addValue:[NSString stringWithFormat:@"%i", bodyString.length] forHTTPHeaderField:@"Content-Length"];
+    [courseInfoRequest addValue:action forHTTPHeaderField:@"SOAPAction"];
+    [courseInfoRequest setHTTPMethod:@"POST"];
+    [courseInfoRequest setHTTPBody:[bodyString dataUsingEncoding:NSUTF8StringEncoding]];
+    
+    self.urlConnection = [[NSURLConnection alloc] initWithRequest:courseInfoRequest delegate:self startImmediately:YES];
+}
+
 
 -(void)getL2PCourseRooms
 {
@@ -88,71 +131,6 @@ typedef enum _requestState requestState;
     self.urlConnection = [[NSURLConnection alloc] initWithRequest:courseInfoRequest delegate:self startImmediately:YES];
 }
 
--(void)getWikiPages
-{
-    self.state = wikiPage;
-    
-    NSString *url = @"https://www2.elearning.rwth-aachen.de/L2PWebservices/L2PSharedDomainService.asmx";
-    NSString *bodyString = [NSString stringWithFormat:@"<?xml version=\"1.0\" encoding=\"utf-8\"?><soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\"><soap:Body><GetWikiPages xmlns=\"http://cil.rwth-aachen.de/l2p/services\"><userToken>%@</userToken><courseId>%s</courseId></GetWikiPages></soap:Body></soap:Envelope>", self.accessToken, iPhoneCourseID];
-    
-    NSMutableURLRequest *wikiPageRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
-    [wikiPageRequest addValue:@"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-    [wikiPageRequest addValue:[NSString stringWithFormat:@"%i", bodyString.length] forHTTPHeaderField:@"Content-Length"];
-    [wikiPageRequest addValue:@"http://cil.rwth-aachen.de/l2p/services/GetWikiPages" forHTTPHeaderField:@"SOAPAction"];
-    [wikiPageRequest setHTTPMethod:@"POST"];
-    [wikiPageRequest setHTTPBody:[bodyString dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    self.urlConnection = [[NSURLConnection alloc] initWithRequest:wikiPageRequest delegate:self startImmediately:YES];
-}
-
--(void)getDiscussions
-{
-    self.state = discussion;
-    
-    NSString *url = @"https://www2.elearning.rwth-aachen.de/L2PWebservices/L2PSharedDomainService.asmx";
-    NSString *bodyString = [NSString stringWithFormat:@"<?xml version=\"1.0\" encoding=\"utf-8\"?><soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\"><soap12:Body><GetThreads xmlns=\"http://cil.rwth-aachen.de/l2p/services\"><userToken>%@</userToken>                      <courseId>%s</courseId></GetThreads></soap12:Body></soap12:Envelope>", self.accessToken, iPhoneCourseID];
-    
-    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
-    [urlRequest addValue:@"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-    [urlRequest addValue:[NSString stringWithFormat:@"%i", bodyString.length] forHTTPHeaderField:@"Content-Length"];
-    [urlRequest setHTTPMethod:@"POST"];
-    [urlRequest setHTTPBody:[bodyString dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    self.urlConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self startImmediately:YES];
-}
-
--(void)getDocuments
-{
-    self.state = documents;
-    
-    NSString *url = @"https://www2.elearning.rwth-aachen.de/L2PWebservices/L2PLearningMaterialService.asmx";
-    NSString *bodyString = [NSString stringWithFormat:@"<?xml version=\"1.0\" encoding=\"utf-8\"?><soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\"><soap12:Body><GetDocumentsOverview xmlns=\"http://cil.rwth-aachen.de/l2p/services\"><userToken>%@</userToken><courseId>%s</courseId></GetDocumentsOverview></soap12:Body></soap12:Envelope>", self.accessToken, iPhoneCourseID];
-    
-    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
-    [urlRequest addValue:@"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-    [urlRequest addValue:[NSString stringWithFormat:@"%i", bodyString.length] forHTTPHeaderField:@"Content-Length"];
-    [urlRequest setHTTPMethod:@"POST"];
-    [urlRequest setHTTPBody:[bodyString dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    self.urlConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self startImmediately:YES];
-}
-
--(void)getPDFWithID:(NSString*)documentID
-{
-    self.state = pdf;
-    
-    NSString *url = @"https://www2.elearning.rwth-aachen.de/L2PWebservices/L2PLearningMaterialService.asmx";
-    NSString *bodyString = [NSString stringWithFormat:@"<?xml version=\"1.0\" encoding=\"utf-8\"?><soap12:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap12=\"http://www.w3.org/2003/05/soap-envelope\"><soap12:Body><DownloadDocumentItem xmlns=\"http://cil.rwth-aachen.de/l2p/services\"><userToken>%@</userToken><courseId>%s</courseId><fileId>%@</fileId></DownloadDocumentItem></soap12:Body></soap12:Envelope>", self.accessToken, iPhoneCourseID, documentID];
-    
-    NSMutableURLRequest *urlRequest = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:url]];
-    [urlRequest addValue:@"text/xml; charset=utf-8" forHTTPHeaderField:@"Content-Type"];
-    [urlRequest addValue:[NSString stringWithFormat:@"%i", bodyString.length] forHTTPHeaderField:@"Content-Length"];
-    [urlRequest setHTTPMethod:@"POST"];
-    [urlRequest setHTTPBody:[bodyString dataUsingEncoding:NSUTF8StringEncoding]];
-    
-    self.urlConnection = [[NSURLConnection alloc] initWithRequest:urlRequest delegate:self startImmediately:YES];
-}
-
 #pragma mark NSURLConnectionDataDelegate
 
 -(void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data
@@ -163,6 +141,9 @@ typedef enum _requestState requestState;
         [self.xmlParser setDelegate:self];
         [self.xmlParser setShouldProcessNamespaces:YES];
         [self.xmlParser parse];
+        
+        
+        //NSLog(@"response: %@", [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding]);
     }
 }
 
@@ -187,85 +168,42 @@ typedef enum _requestState requestState;
 -(void)parser:(NSXMLParser *)parser didStartElement:(NSString *)elementName namespaceURI:(NSString *)namespaceURI qualifiedName:(NSString *)qName attributes:(NSDictionary *)attributeDict
 {
     self.elementNameFromXMLParser = elementName;
+    
+    // For each element Appointment, get the attributes that contain course's starting and ending times.
+    if (self.state == courseDates && [elementName isEqualToString:@"Appointment"] )
+    {
+        //NSLog(@"Attributes: %@", attributeDict);
+        NSDictionary* dateDict = @{@"start": attributeDict[@"start"],
+                                   @"end": attributeDict[@"end"]
+                                   };
+        
+        //[dates addObject:dateDict];
+        NSLog(@"dates: %@", dateDict);
+    }
 }
 
 -(void)parser:(NSXMLParser *)parser foundCharacters:(NSString *)string
 {
-    string = [string stringByAppendingString:@"\n"];
-    NSString *stringToAppend = nil;
+    if (self.state == courseRooms && [self.elementNameFromXMLParser isEqualToString:@"ID"])
+    {
+        [IDs addObject:string];
+    }
+}
+
+- (void)parserDidEndDocument:(NSXMLParser *)parser
+{
+    //NSLog(@"IDs: %@", IDs);
     
-    switch (self.state) {
-        case wikiPage:
-            if ([self.elementNameFromXMLParser isEqualToString:@"WikiInfo"]) {
-                stringToAppend = @"\nWiki";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"Body"]) {
-                stringToAppend = @"Body: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"ID"]) {
-                stringToAppend = @"ID: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"Name"]) {
-                stringToAppend = @"Name: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"ShowInSummary"]) {
-                stringToAppend = @"ShowInSummary: ";
-            }
-            stringToAppend = [stringToAppend stringByAppendingString:string];
-            break;
-        case discussion:
-            if ([self.elementNameFromXMLParser isEqualToString:@"ThreadInfo"]) {
-                stringToAppend = @"\nThread";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"ID"]) {
-                stringToAppend = @"ID: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"Title"]) {
-                stringToAppend = @"Title: ";
-            }
-            stringToAppend = [stringToAppend stringByAppendingString:string];
-            break;
-        case announcement:
-            //
-            break;
-        case courseRooms:
-            if ([self.elementNameFromXMLParser isEqualToString:@"CourseType"]) {
-                stringToAppend = @"\nCourseRoomInfo\nCourseType: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"FullUrl"]) {
-                stringToAppend = @"FullUrl: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"ID"]) {
-                stringToAppend = @"ID: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"MasterId"]) {
-                stringToAppend = @"MasterId: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"Status"]) {
-                stringToAppend = @"Status: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"Title"]) {
-                stringToAppend = @"Title: ";
-            }
-            stringToAppend = [stringToAppend stringByAppendingString:string];
-            break;
-        case documents:
-            if ([self.elementNameFromXMLParser isEqualToString:@"DocumentInfo"]) {
-                stringToAppend = @"\nWiki";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"Id"]) {
-                stringToAppend = @"ID: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"Name"]) {
-                stringToAppend = @"Name: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"Url"]) {
-                stringToAppend = @"Url: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"LastUpdated"]){
-                stringToAppend = @"LastUpdated: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"FileType"]){
-                stringToAppend = @"FileType: ";
-            } else if ([self.elementNameFromXMLParser isEqualToString:@"FileSize"]){
-                stringToAppend = @"FileSize: ";
-            }
-            stringToAppend = [stringToAppend stringByAppendingString:string];
-            break;
-        default:
-            stringToAppend = @"unknown state";
-            break;
+    if (self.state == courseRooms)
+    {
+        for (NSString* courseID in IDs)
+        {
+            [self getDatesForL2PCourse:courseID];
+        }
+        
+        [IDs removeAllObjects];
     }
     
-    if (stringToAppend != nil) {
-        
-        NSLog(@"Fetched data: %@", stringToAppend);
-        
-    }
 }
 
 -(void)parserDidStartDocument:(NSXMLParser *)parser
