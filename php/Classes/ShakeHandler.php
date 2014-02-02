f5668417077f8038ca782d6b86ee7331486fdd18
yigitgunay <yigit.guenay@rwth-aachen.de>
2014-02-02 23:50:09 +0100

    Little update on shake action

--- php/Classes/ShakeHandler.php
+++ php/Classes/ShakeHandler.php
@@ -7,12 +7,15 @@ class ShakeHandler extends ActionHandler
 		$userID = $this->getUserIDIfExists($this->options["username"]);
 		$latitude = $this->mysqli->real_escape_string($this->options["latitude"]);
 		$longitude = $this->mysqli->real_escape_string($this->options["longitude"]);
-		$timestamp = $this->mysqli->real_escape_string($this->options["timestamp"]);
+		$timestamp = time();
 		if ($userID > 0)
 		{
 			$query = "INSERT INTO shakes (userID, latitude, longitude, timestamp) VALUES ($userID, '$latitude', '$longitude', '$timestamp')";
 			$res = $this->mysqli->query($query);
+			if ($res)
+				return array("response" => "success");
 		}
+		return array("response" => "failure");
 	}
 }
 
